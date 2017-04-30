7d27e94839b3613d7749068ef7894b3d5808ce0b
Pavlo <p.bazyliuk@gmail.com>
2017-04-30 19:12:47 +0300

    added jwt and local strategies with passport

--- models/user.js
+++ models/user.js
@@ -2,37 +2,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt-nodejs');
 
-//Define user model
+// Define our model
 const userSchema = new Schema({
-    email: { type: String, unique: true, lowercase: true },
-    password: String
+  email: { type: String, unique: true, lowercase: true },
+  password: String
 });
 
-//On Save, encrypt password
-
-//Before saving a model run this function
+// On Save Hook, encrypt password
+// Before saving a model, run this function
 userSchema.pre('save', function(next) {
-    //Get access to the user model
-    const user = this;
+  // get access to the user model
+  const user = this;
 
-    //generate a salt and then run callback
-    bcrypt.genSalt(10, function(err, salt) {
-        if(err) return next(err);
+  // generate a salt then run callback
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
 
-        //hash (encrypt) our password using the salt
-        bcrypt.hash(user.password, salt, null, function(err, hash) {
-            if(err) return next(err);
+    // hash (encrypt) our password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
 
-            //overwrite plain text password with encrypted password
-            user.password = hash;
-            next();
-        })
+      // overwrite plain text password with encrypted password
+      user.password = hash;
+      next();
     });
+  });
 });
 
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) { return callback(err); }
 
-//Create the model class
+    callback(null, isMatch);
+  });
+}
+
+// Create the model class
 const ModelClass = mongoose.model('user', userSchema);
 
-//Export the model
+// Export the model
 module.exports = ModelClass;
