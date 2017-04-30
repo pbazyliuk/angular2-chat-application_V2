2ab6224fce8ced7e2e6b041baeeef97ee2433927
Pavlo <p.bazyliuk@gmail.com>
2017-04-30 16:07:54 +0300

    added password hashing

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -4,6 +4,9 @@ exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
 
+    if(!email || !password) {
+        return res.status(422).send({ error: 'You must provide email and password!'})
+    }
     // See if a user with the given email exists
     User.findOne({ email : email }, function(err, existingUser) {
         if(err) {
@@ -23,7 +26,7 @@ exports.signup = function(req, res, next) {
             if(err) return next(err);
             
             //Respond to request indication the user was created    
-            res.json({ success: true });
+            res.json(user);
         });
     });
 } 
--- models/user.js
+++ models/user.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 //Define user model
 const userSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
     password: String
 });
+
+//On Save, encrypt password
+userSchema.pre('save', function(next) {
+    const user = this;
+
+    bcrypt.genSalt(10, function(err, salt) {
+        if(err) return next(err);
+
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if(err) return next(err);
+
+            user.password = hash;
+            next();
+        })
+    });
+});
+
 
 //Create the model class
 const ModelClass = mongoose.model('user', userSchema);
