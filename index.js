680471c8ab7da17d52512bfdd2c76c3c930ca997
Pavlo <p.bazyliuk@gmail.com>
2017-04-30 14:28:42 +0300

    added test route

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router')
 
 //App Setup
 app.use(morgan('combined')); //login request framework
 app.use(bodyParser.json({ type: '*/*' })) //parser incoming request
+router(app);
 
 //Server Setup
 const port = process.env.PORT || 8090;
