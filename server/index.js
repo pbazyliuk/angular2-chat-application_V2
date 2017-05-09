45df00bb4b62031514a1a0af2ea3ea798b39c999
Pavlo <p.bazyliuk@gmail.com>
2017-05-09 13:08:56 +0300

    added websockets to server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const router = require('./router');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 //DB Setup
 mongoose.connect('mongodb://localhost:auth/auth');
 
@@ -17,9 +19,21 @@ app.use(morgan('combined')); //login request framework
 app.use(bodyParser.json({ type: '*/*' })) //parser incoming request
 router(app);
 
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+  socket.on('add-message', (message) => {
+    io.emit('message', {type:'new-message', text: message});    
+  });
+});
+
 //Server Setup
 const port = process.env.PORT || 8090;
 
-const server = http.createServer(app);
 server.listen(port);
 console.log('Server is listening on: ', port)
