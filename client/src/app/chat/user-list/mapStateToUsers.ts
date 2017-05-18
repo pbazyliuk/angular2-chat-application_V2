b47107e1b1c1fe779d56f14b7d3ea93e9b375101
Pavlo <p.bazyliuk@gmail.com>
2017-05-18 16:02:50 +0300

    added users on & off value in user list

--- client/src/app/chat/user-list/mapStateToUsers.ts
+++ client/src/app/chat/user-list/mapStateToUsers.ts
@@ -4,5 +4,5 @@ import { ApplicationState } from "app/store/application-state";
 import { User } from "app/core/models/user";
 
 export function mapStatetoUsers(state: ApplicationState) { //: User[] 
-  return state.uiState.usersOn;
+  return state.storeData.users;
 }
